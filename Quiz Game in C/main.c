6d7c06499ce6b19f2db2c0a4079d94f07e9db2ff
Matt <matthew.lubker@gmail.com>
2025-01-13 14:03:43 -0500

    Update main.c

--- Quiz Game in C/main.c
+++ Quiz Game in C/main.c
@@ -1,5 +1,5 @@
 
-
+/*
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
@@ -93,3 +93,114 @@ int main(void) {
 
     return 0;
 }
+*/
+/*
+#include <stdio.h>
+
+#define MAX_QUESTION 256
+#define MAX_ANSWER 128
+
+typedef struct 
+{
+    char question[MAX_QUESTION];
+    char answerA[MAX_ANSWER];
+    char answerB[MAX_ANSWER];
+    char answerC[MAX_ANSWER];
+    char answerD[MAX_ANSWER];
+    char correct_answer;
+} quiz_question;
+
+int main(void)
+{
+    quiz_question quiz[TOTAL_QUESTIONS];
+
+    strcpy(quiz[0].question,"What dos the ++ operator do?");
+    strcpy(quiz[0].answerA,"Add two");
+    strcpy(quiz[0].answerB,"");
+    strcpy(quiz[0].answerC,"");
+    strcpy(quiz[0].answerD,"");
+    quiz[0].correct_answer = 'B';
+
+    double total_corret = 0;
+    char answer;
+
+    for (int i 0; 1 < TOTAL_QUESTIONS; i++)
+    {
+        printf("Question %d: % \n\n", (i+1+), quiz[i].question);
+        printf("A) %s\n", quiz[i].answerA);
+        printf("B) %s\n", quiz[i].answerB);
+        printf("C) %s\n", quiz[i].answerC);
+        printf("D) %s\n", quiz[i].answerD);
+        printf("\nEnter Answer (A, B,C or D): ");
+
+
+        scanf("% c", &answer);
+
+        if (toupper(answer) == quiz[i].correct_answer)
+        {
+            total_corret++;
+            printf("\n\nCorrect Answer!");
+
+        }
+        else
+        {
+            printf("\n\nIncorrect Answer!");
+            printf("\n\nThe correct answer was %C.",
+                quiz[i].correct_answer);
+        
+        }
+
+            printf("\n\n\n");
+
+        }
+
+        printf("%d%d question answered correctly",(int) total_corret. 
+        TOTAL_QUESTIONS);
+
+        printf("(%.2f%%)",
+        ((total_corret / TOTAL_QUESTIONS)* 100))
+
+
+
+    }
+
+    return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
